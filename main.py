d1c488cec8d8e55057a6e992dd32412309593574
sameertak <67770971+sameertak@users.noreply.github.com>
2021-06-30 11:19:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+#Works in the Terminal
 import sys
 from PIL import Image
 import os
 
-loc1 = sys.argv[1]
-loc2 = sys.argv[2]
+loc1 = sys.argv[1]      #Name of the folder - 'folder_name/'    (where jpg images are being stored)
+loc2 = sys.argv[2]      #Name of the folder - 'folder_name/'    (where png images are going to be stored)
 
-if not os.path.exists(loc2):
+if not os.path.exists(loc2):    #If folder doesn't exists, it'll create a new.
     os.makedirs(loc2)
 
 for filename in os.listdir(loc1):
